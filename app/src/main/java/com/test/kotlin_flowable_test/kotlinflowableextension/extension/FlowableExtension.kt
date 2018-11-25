bffa53f87c6a3f69b59cb454f0a131520388c2c2
pavel.petrov <pppetrv@gmail.com>
2018-11-25 14:29:20 +0200

    improved extension functions logic

--- app/src/main/java/com/test/kotlin_flowable_test/kotlinflowableextension/extension/FlowableExtension.kt
+++ app/src/main/java/com/test/kotlin_flowable_test/kotlinflowableextension/extension/FlowableExtension.kt
@@ -3,58 +3,16 @@ package com.test.kotlin_flowable_test.kotlinflowableextension.extension
 import com.test.kotlin_flowable_test.kotlinflowableextension.utils.LifecycleManager
 import com.test.kotlin_flowable_test.kotlinflowableextension.utils.getCurrentTimeMillis
 import io.reactivex.Flowable
-import io.reactivex.FlowableSubscriber
-import io.reactivex.Scheduler
-import io.reactivex.schedulers.Schedulers
 import java.io.IOException
-import java.util.concurrent.TimeUnit
-
-/**
- *
- *  @param retryExceptions list of exception classes that method will handle
- *  @param retryMaxTimes number, default: 3 tries
- *  @param maxDelayTime milliseconds, default: 30000 (30 sec)
- *  @return the new Flowable instance
- */
-fun <R> Flowable<R>.retryOnError(
-    vararg retryExceptions: Class<Throwable>,
-    retryMaxTimes: Long = 3,
-    maxDelayTime: Long = 30000): Flowable<R> {
-    val timestamp = Timestamp()
-    var first = true
-    return timeout(maxDelayTime, TimeUnit.SECONDS)
-        .retry(retryMaxTimes) { error ->
-        val catchException = error.javaClass in retryExceptions
-        var retry = catchException
-        if (catchException) {
-            println("retry block, error: $error")
-            if (first) {
-                timestamp.reset()
-                first = false
-                println("retry block, init timestamp: $timestamp")
-            }
-            val timeElapsed = timestamp.getTime()
-            println("retry block, timeElapsed: $timeElapsed")
-            if (timeElapsed >= maxDelayTime) {
-                println("retry block, timeout, time exceeded")
-                retry = false
-            }
-        } else {
-            println("retry block, unmanageable exception: $error")
-        }
-        retry
-    }
-}
 
 /**
  *
  *  @param retryExceptions list of exception classes that method will handle
  *  @param retryMaxTimes number, default: 3 tries
  *  @param maxDelayTime seconds, default: 30 sec
- *  @return the new Flowable instance
+ *  @return the new Flowable instance with retry on errors support
  */
-fun <R> Flowable<R>.retryOnError1(
-    vararg retryExceptions: Class<Throwable>,
+fun <R> Flowable<R>.retryOnError(retryExceptions: Array<Class<Throwable>>,
     retryMaxTimes: Long = 3,
     maxDelayTime: Long = 30): Flowable<R> {
     val timestamp = Timestamp()
@@ -69,27 +27,39 @@ fun <R> Flowable<R>.retryOnError1(
                 println("init timestamp: $timestamp")
             }
             val timeElapsed = timestamp.getTime()
-            println("timeElapsed: $timeElapsed")
-            if (count < retryMaxTimes - 1 && isErrorHandled
-                && timeElapsed <= maxDelayTime * 1000) {
-                count++
-                println("handle IOException, count: $count")
-                Flowable.just { null }
-            } else {
-                println("unhandled exception or count: $count, error: $error")
-                Flowable.error { error }
+            println("timeElapsed: $timeElapsed ms")
+            if (isErrorHandled ) { count++ }
+            when {
+                timeElapsed >= maxDelayTime * 1000 -> {
+                    println("timeout, timeElapsed: $timeElapsed ms")
+                    Flowable.error { error }
+                }
+                count <= retryMaxTimes && isErrorHandled -> {
+                    println("handling ${error::class.java.simpleName}, count: $count")
+                    Flowable.just { null }
+                }
+                else -> {
+                    println("unhandled exception or max count: $count, exception: ${error::class.java.simpleName}")
+                    Flowable.error { error }
+                }
             }
         }
     }
 }
 
-fun <R> Flowable<R>.subscribeAndRetryOnIOError(subscriber: FlowableSubscriber<R>,
-                                               manager: LifecycleManager,
-                                               scheduler: Scheduler = Schedulers.trampoline()) {
+/**
+ *
+ *  @param manager: LifecycleManager instance of class that is responsive
+ *  for pause/resume Flowable emitting depending on Android UI lifecycle events
+ *  @param retryMaxTimes number, default: 3 tries
+ *  @param maxDelayTime seconds, default: 30 sec
+ *  @return the new Flowable instance with UI lifecycle control support
+ */
+fun <R> Flowable<R>.lifecycleAndRetryOnIOError(manager: LifecycleManager,
+    retryMaxTimes: Long = 3, maxDelayTime: Long = 30): Flowable<R> {
     val sourceValved = manager.attachValve(this)
-    sourceValved.retryOnError1(IOException::class.java as Class<Throwable>)
-        .subscribeOn(scheduler)
-        .subscribe(subscriber)
+    return sourceValved.retryOnError(
+        arrayOf(IOException::class.java as Class<Throwable>), retryMaxTimes, maxDelayTime)
 }
 
 class Timestamp {
--- app/src/main/java/com/test/kotlin_flowable_test/kotlinflowableextension/utils/LifecycleManager.kt
+++ app/src/main/java/com/test/kotlin_flowable_test/kotlinflowableextension/utils/LifecycleManager.kt
@@ -3,26 +3,23 @@ package com.test.kotlin_flowable_test.kotlinflowableextension.utils
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.OnLifecycleEvent
-import io.reactivex.processors.BehaviorProcessor
 import io.reactivex.Flowable
 
 class LifecycleManager: LifecycleObserver {
 
     var initialState = true
-    private var lifecycleProcessor = BehaviorProcessor.create<Boolean>()
-    private var openValve: Flowable<Boolean>
-
-    init {
-        openValve = lifecycleProcessor
-            .filter { v -> v }
-            .take(1)
-    }
+    @Volatile var isPaused = false
 
     fun <R> attachValve(flow: Flowable<R>): Flowable<R> {
         val valved = flow
-            .concatMap { i ->
-                openValve.map { v -> i }
+        .distinctUntilChanged()
+        .switchMap { item ->
+            if (!isPaused) {
+                Flowable.just(item)
+            } else {
+                Flowable.empty()
             }
+        }
         initState()
         return valved
     }
@@ -45,19 +42,13 @@ class LifecycleManager: LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
-        lifecycleProcessor.onNext(true)
+        isPaused = false
         println("LifecycleManager, received ON_RESUME")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
-        lifecycleProcessor.onNext(false)
+        isPaused = true
         println("LifecycleManager, received ON_PAUSE")
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun onDestroy() {
-        lifecycleProcessor.onComplete()
-        println("LifecycleManager, received ON_DESTROY")
     }
 }
